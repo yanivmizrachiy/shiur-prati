5ad8c480d64d02757ccb991d44b838c9df0091f5
Yaniv Raz <yanivmiz77@gmail.com>
2026-06-10 08:48:34 +0300

    feat: add U7-01 frequency slice

---
+++ generator/u7-01.js
@@ -0,0 +1,2 @@
+TOPICS[7].uncertainty.push(['U7-01','Frequency table',1]);
+generators['U7-01']=function(){renderCard('U7-01','Frequency table','','$80,70,90,80,60$','60:1\\n70:1\\n80:2\\n90:1','unc')};
