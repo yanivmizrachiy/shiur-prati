6af14923e233c5da7ce75e5cb15c799d296ac274
Yaniv Raz <yanivmiz77@gmail.com>
2026-06-10 09:19:46 +0300

    feat: add G8-04 similarity slice

---
+++ generator/g8-04.js
@@ -0,0 +1,2 @@
+TOPICS[8].geometry.push(['G8-04','Similarity',1]);
+generators['G8-04']=function(){let q='Two similar triangles have ratio $2:3$. A side in the small triangle is $8$. Find the matching side in the large triangle.',a='The scale factor is $\\frac{3}{2}$.\n$$8\\times\\frac{3}{2}=12$$';renderCard('G8-04','Similarity','',q,a,'geo')};
