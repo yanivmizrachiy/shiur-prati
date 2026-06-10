9c8ab4ece5b530d010d66f8c26e6ad0facbaf0ef
Yaniv Raz <yanivmiz77@gmail.com>
2026-06-10 08:52:14 +0300

    fix: localize N8-01 ratio slice

--- generator/n8-ratio.js
+++ generator/n8-ratio.js
@@ -1,2 +1,2 @@
-TOPICS[8].numeric.push(['N8-01','Ratio',1]);
-generators['N8-01']=function(){renderCard('N8-01','Ratio','','$18:24$','$$18:24=3:4$$','num')};
+TOPICS[8].numeric.push(['N8-01','\u05d9\u05d7\u05e1',1]);
+generators['N8-01']=function(){let q='\u05de\u05e6\u05d0\u05d5 \u05d9\u05d7\u05e1 \u05de\u05e6\u05d5\u05de\u05e6\u05dd: $18:24$',a='\u05de\u05d7\u05dc\u05e7\u05d9\u05dd \u05d0\u05ea \u05e9\u05e0\u05d9 \u05d7\u05dc\u05e7\u05d9 \u05d4\u05d9\u05d7\u05e1 \u05d1-$6$.\n$$18:24=3:4$$';renderCard('N8-01','\u05d9\u05d7\u05e1','',q,a,'num')};
