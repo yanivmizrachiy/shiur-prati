65cbbc9c10c4fe49a2d7e9234232e8bce97aa954
Yaniv Raz <yanivmiz77@gmail.com>
2026-06-10 08:47:25 +0300

    feat: finalize N8-01 ratio slice

--- generator/n8-ratio.js
+++ generator/n8-ratio.js
@@ -1,2 +1,2 @@
-TOPICS[8].numeric.push(['N8-RATIO','Ratio',1]);
-generators['N8-RATIO']=function(){renderCard('N8-RATIO','Ratio','','$18:24$','$$18:24=3:4$$','num')};
+TOPICS[8].numeric.push(['N8-01','Ratio',1]);
+generators['N8-01']=function(){renderCard('N8-01','Ratio','','$18:24$','$$18:24=3:4$$','num')};
